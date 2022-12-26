99ba87c6bf66e6ca811a649f1fe8ac3018fdf1f5
Waleed Hussein <62118101+Waleed405@users.noreply.github.com>
2022-12-27 00:15:32 +0200

    init app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,32 @@
-console.log("Hello world.")
+function Gameboard () {
+  this.gameboard = [];
+};
+
+const Player = (type) => {
+  return {type};
+};
+
+const control = (() => {
+  const turn = 0;
+
+  const checkForWin = () => {
+    //code win conditions
+
+    return false;
+  }
+
+  return {
+    turn,
+    checkForWin
+  };
+})();
+
+const DOMControl = (() => {
+  const addEventListeners = () => {
+    // code here
+  };
+
+  return {
+    addEventListeners
+  }
+});
